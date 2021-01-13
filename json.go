a70a6a738617f4b221159d336f0213d506e6a9b4
eltacohero <jrm.grondin@protonmail.com>
2021-01-13 21:05:27 +0100

    add array & 2 entries

--- json.go
+++ json.go
@@ -5,19 +5,33 @@ import (
 	"encoding/json"
 )
 
-type Users struct {
+type User struct {
 	ID int
 	Login string `json:"Username"`
 	Password string
-} 
+}
+
+type Users []User
 
 func main() {
-	group := Users{
+	users := Users{
+	User{
 		ID: 1,
 		Login: "Paul",
 		Password: "pass123",
-	}
-	b, err := json.Marshal(group)
+	},
+	User{
+		ID: 2,
+		Login: "matm",
+		Password: "123456",
+	},
+	User{
+		ID: 3,
+		Login: "fake44",
+		Password: "azerty",
+	},
+}
+	b, err := json.Marshal(users)
 	if err != nil{
 		fmt.Println("error", err)
 	}
