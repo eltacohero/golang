bbcd5aeacb4d60f68a69d79dfab2ece58b8037e5
eltacohero <jrm.grondin@protonmail.com>
2021-01-13 09:50:04 +0100

    stringer

--- td2.go
+++ td2.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type IPAddr [4]byte
+
+func (ipAddr IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ipAddr[0], ipAddr[1],ipAddr[2],ipAddr[3])
+}
 
 func main () {
 	hosts := map[string]IPAddr{
