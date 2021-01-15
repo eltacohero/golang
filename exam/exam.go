85cd8975154d4a8c7c86f7c1a779f34f40ada731
eltacohero <jrm.grondin@protonmail.com>
2021-01-15 16:38:00 +0100

    exam.go

--- exam/exam.go
+++ exam/exam.go
@@ -3,31 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
-
+//define Task type structure
 type Task struct {
 	ID int
 	Description int
 	Done bool
 }
-
+//define task var as a slice of Task
 var task []Task
-
+//main function
 func main() {
 	http.HandleFunc("/", list)
 	http.HandleFunc("/done", done)
 	http.HandleFunc("/add", add)
 	http.ListenAndServe(":8080", nil)
 }
-
+//list function
 func list(rw http.ResponseWriter, _ *http.Request) {
 	for i, v := range task {
-		fmt.Println(i, v)
+		if Done == false {
+			fmt.Println(i, v)
+		}
 	}
 	rw.WriteHeader(http.StatusOK)
 }
-
-func done(d http.ResponseWriter, _ *http.Request) {
+//done function
+func done(rw http.ResponseWriter, _ *http.Request) {
 }
-func add(a http.ResponseWriter, _ *http.Request) {
+//add function
+func add(rw http.ResponseWriter, _ *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("Error reading body:%v", err)
+		http.Error(
+		rw,
+		"can't read body", http.StatusBadRequest,
+		)
+		return
+	}
 }
