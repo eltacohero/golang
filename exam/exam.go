243fac9e38913f1bad5bb199231043ac5fbb7a3b
eltacohero <jrm.grondin@protonmail.com>
2021-01-15 16:20:19 +0100

    add exam.go

--- exam/exam.go
+++ exam/exam.go
@@ -2,5 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+type Task struct {
+	ID int
+	Description int
+	Done bool
+}
+
+var task []Task
+
+func main() {
+	http.HandleFunc("/", list)
+	http.HandleFunc("/done", done)
+	http.HandleFunc("/add", add)
+	http.ListenAndServe(":8080", nil)
+}
+
+func list(rw http.ResponseWriter, _ *http.Request) {
+	for i, v := range task {
+		fmt.Println(i, v)
+	}
+	rw.WriteHeader(http.StatusOK)
+}
+
+func done(d http.ResponseWriter, _ *http.Request) {
+}
+func add(a http.ResponseWriter, _ *http.Request) {
+}
